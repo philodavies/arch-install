f35b2746757c6191e54d4d86472941f93d6a08f4
Phil <40214783+philodavies@users.noreply.github.com>
2021-05-04 23:00:38 -0400

    Fix quotes preventing creation of multiple dirs in curly brace

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # Create user's home dirs
-sudo -u "$name" mkdir "/home/$name/{Documents,Pictures,Videos,Downloads}"
+sudo -u "$name" mkdir "/home/$name/"{Documents,Pictures,Videos,Downloads}
 
 # Make zsh the default shell for the user.
 chsh -s /bin/zsh "$name" >/dev/null 2>&1
